7c6e11ef1aa9442765bc230d5b6320c3698080e3
isurumanoddev <isurumanod99@gmail.com>
2023-01-20 04:27:13 +0530

    Creates frames main window

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import settings
+import utils
+
+window = Tk()
+window.configure(bg="black")
+window.geometry(f"{settings.WIDTH}x{settings.HEIGHT}")
+window.title("Minesweeper Game")
+window.resizable(False, False)
+frame = Frame(window, bg="brown", width=settings.WIDTH, height=utils.height_percentage(25))
+frame.place(x=0, y=0)
+left_frame = Frame(window, width=150, height=utils.height_percentage(75), bg="green")
+left_frame.place(x=0, y=180)
+
+window.mainloop()
+
--- test.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-window = Tk()
-window.configure(bg="black")
-window.geometry("900x600")
-window.title("Minesweeper Game")
-window.resizable(False,False)
-window.mainloop()
