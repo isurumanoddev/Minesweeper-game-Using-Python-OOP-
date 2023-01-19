34849affeebfbccc5b1839600aed887e255807b0
isurumanoddev <isurumanod99@gmail.com>
2023-01-20 04:27:13 +0530

    First create GUI via Tkinter

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 window = Tk()
 window.configure(bg="black")
-window.geometry("900x600")
+window.geometry("800x600")
 window.title("Minesweeper Game")
 window.resizable(False,False)
 window.mainloop()
