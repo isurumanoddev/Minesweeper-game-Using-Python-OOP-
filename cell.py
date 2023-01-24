8c0c850c66a89f92776dfba96e5f291a06be4940
isurumanoddev <111575402+isurumanoddev@users.noreply.github.com>
2023-01-24 05:31:38 +0530

    complete 90%

--- cell.py
+++ cell.py
@@ -6,36 +6,46 @@ import ctypes
 
 class Cell:
     all = []
-    ramaining_cell_label =None
-
+    ramaining_cell_label = None
+    ramaining_mine_candidate_label = None
+    remaining_cell_count = 99
+    ramaining_mine_candidate_count = 15
 
     def __init__(self, x, y, is_mine=False):
         self.y = y
         self.x = x
+        self.is_open = False
         self.is_mine = is_mine
+        self.is_mine_candidate = False
         self.button_object = None
         Cell.all.append(self)
 
     def create_btn_object(self, location):
-        button = Button(location, text="", font=("", 12), width=9, height=2, fg="white", bg="black")
+        button = Button(location, text="", font=("", 14), width=5, height=1, fg="white", bg="black")
         self.button_object = button
         button.bind("<Button-1>", self.click_left_actions)
         button.bind("<Button-3>", self.click_right_actions)
 
     def click_left_actions(self, event):
+        if not self.is_mine_candidate:
 
-        if self.is_mine:
-            self.show_mine()
-        else:
-            self.show_cell()
-            if self.surrounded_cells_mines_lenght() == 0:
-                for cell in self.surrounded_cell:
-                    cell.show_cell()
+            if self.is_mine:
+                self.show_mine()
+            else:
+                self.show_cell()
+                if self.surrounded_cells_mines_lenghth() == 0:
+                    for cell in self.surrounded_cell:
+                        cell.show_cell()
 
     @staticmethod
-    def cell_count_label(self,location):
-        label = Label(location, text="Reamining cells 64", font=("", 12), width=9, height=2, fg="white", bg="black")
-        Cell.ramaining_cell_label =label
+    def cell_count_label(location):
+        label = Label(location, text="Reamining cells : 99", font=("", 12), width=40, height=2, fg="white", bg="green")
+        Cell.ramaining_cell_label = label
+
+    @staticmethod
+    def mine_candidate_count_label(location):
+        label = Label(location, text="Reamining mines : 15", font=("", 12), width=40, height=2, fg="white", bg="green")
+        Cell.ramaining_mine_candidate_label = label
 
     # def check_mines(self):
     #     for cell in self.surrounded_cell:
@@ -46,17 +56,30 @@ class Cell:
         self.button_object.config(bg="red", fg="black")
 
     def show_cell(self):
-        self.button_object.config(bg="yellow", fg="black",text=f"{self.surrounded_cells_mines_lenght()}")
 
-    def surrounded_cells_mines_lenght(self):
+        if self.is_open == False:
+            Cell.remaining_cell_count -= 1
+            print(Cell.ramaining_cell_label)
+            self.button_object.config(bg="yellow", fg="black", text=f"{self.surrounded_cells_mines_lenghth()}")
+            Cell.ramaining_cell_label.config(text=f"Reamining cells {Cell.remaining_cell_count}")
+            self.is_open = True
+
+    def surrounded_cells_mines_lenghth(self):
         mine_count = 0
         for mine in self.surrounded_cell:
             if mine.is_mine:
-                mine_count +=1
+                mine_count += 1
         return mine_count
 
     def click_right_actions(self, event):
-        self.button_object.config(bg="orange")
+        if not self.is_mine_candidate:
+            if not self.is_open:
+
+                Cell.ramaining_mine_candidate_count -= 1
+                self.button_object.config(bg="orange")
+                Cell.ramaining_mine_candidate_label.config(text=f"Reamining cells {Cell.ramaining_mine_candidate_count}"
+                                                           )
+                self.is_mine_candidate = True
 
     def get_cell_by_axis(self, x, y):
         for cell in Cell.all:
--- main.py
+++ main.py
@@ -10,18 +10,22 @@ window.configure(bg="black")
 window.geometry(f"{settings.WIDTH}x{settings.HEIGHT}")
 window.title("Minesweeper Games")
 window.resizable(False, False)
-frame = Frame(window, bg="#4F1D92", width=settings.WIDTH, height=utils.height_percentage(15))
+frame = Frame(window, bg="black", width=settings.WIDTH, height=utils.height_percentage(25))
 frame.place(x=0, y=0)
-center_frame = Frame(window, bg="#976F0D", width=utils.width_percentage(100), height=utils.height_percentage(85))
-center_frame.place(x=utils.width_percentage(0), y=utils.height_percentage(15))
+center_frame = Frame(window, bg="#976F0D", width=utils.width_percentage(100), height=utils.height_percentage(75))
+center_frame.place(x=utils.width_percentage(0), y=utils.height_percentage(25))
 
-for row in range(8):
-    for column in range(8):
+for row in range(9):
+    for column in range(11):
         c = Cell(row,column)
         c.create_btn_object(center_frame)
         c.button_object.grid(row=row,column =column)
 Cell.random_mines()
 label = Label(frame, bg="black", fg="white", text="Minesweeper Game", font=("", 30))
 label.place(x=150,y=0)
+Cell.cell_count_label(frame)
+Cell.ramaining_cell_label.place(x=0,y=77)
+Cell.mine_candidate_count_label(frame)
+Cell.ramaining_mine_candidate_label.place(x=355,y=77)
 
 window.mainloop()
